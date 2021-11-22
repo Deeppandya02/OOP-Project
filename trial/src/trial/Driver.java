e8ce39faa8da5f0a3f54cdd631e66addc2ad2174
itsharsh334 <80212334+itsharsh334@users.noreply.github.com>
2021-11-22 21:38:24 +0530

    Update Driver.java

--- trial/src/trial/Driver.java
+++ trial/src/trial/Driver.java
@@ -1,4 +1,7 @@
 package trial;
+import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public interface Driver {
 	default Coordinates getCoordinates(Object o) {
@@ -7,8 +10,9 @@ public interface Driver {
 	default Coordinates slide(SlidingObjects obj) {
 		 
 		return obj.getCoordinates();
-	}	
-	
-	
+	}
+	void simulate(int a,int b,Scanner s) throws IOException, Exception;
 }
+
+
 
