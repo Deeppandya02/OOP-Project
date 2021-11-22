12fbbdd19f8aed5c71891f37faab62d467c411e4
Deeppandya02 <74926258+Deeppandya02@users.noreply.github.com>
2021-11-22 15:47:55 +0530

    Update Driver.java

--- trial/src/trial/Driver.java
+++ trial/src/trial/Driver.java
@@ -1,12 +1,46 @@
 package trial;
 
-public interface Driver {
-	default Coordinates getCoordinates(Object o) {
-		return o.getCoordinates(); 
-	}
-	default Coordinates slide(SlidingObjects obj) {
-		 
-		return obj.getCoordinates();
+public class Driver {
+	private double theta;
+	private double g;
+	private double mu;
+	private double e;
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter angle of inclination of Slope");
+	theta = sc.nextDouble();
+	
+	System.out.println("Enter acceleration due to gravity");
+	g = sc.nextDouble();
+	
+	System.out.println("Enter coefficient of friction");
+	mu = sc.nextDouble();
+	
+	System.out.println("Enter coefficient of Restitution");
+	e = sc.nextDouble();
+	
+	Slope slope = new slope(theta,g,mu,e);
+	int n;
+	sc.nextInt();
+	Object[] objs = new Object[n];
+	for(int i = 0; i < n; i++)
+	{
+		String objID;
+		String objTypeID;
+		//Print obj type id for each type
+		objID = sc.nextLine();
+		objID = sc.nextLine();
+		double x,y;
+		x = sc.nextDouble();
+		y = sc.nextDouble();
+		//Orientation;
+		Object obj;
+		switch(objTypeID)
+		{
+			case 0: 
+				obj = new Box();
+				
+		
 	}	
 	
 	
