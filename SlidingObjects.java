f77ebc9e6e65635cfa634b82e18b855ac1046108
Deeppandya02 <74926258+Deeppandya02@users.noreply.github.com>
2021-11-25 19:46:12 +0530

    Update SlidingObjects.java

--- SlidingObjects.java
+++ SlidingObjects.java
@@ -25,13 +25,14 @@ public class SlidingObjects extends Object implements Driver{
 	}
 	public void simulate(int choice,int subchoice,Scanner input, SlidingObjects so2) throws Exception, IOException
 	{
-		
-		double x1,v1,mu,g,x2,v2,v1f,v2f,vcm,m1,m2,e;
+		double x1,y1,v1,mu,g,x2,v2,v1f,vcm,m1,m2,e,v2f,x1AtTime,t1,t2,x2AtTime;
+		System.out.println("Enter the timeslice and total time respectively : ");
 		double ts = input.nextDouble();
 		input.nextLine();
 		double t = input.nextDouble();
 		input.nextLine();
 		x1=super.getCoordinates().getX();
+		y1=super.getCoordinates().getY();
 		x2 = so2.getCoordinates().getX();
 		v1 = this.getV();
 		v2 = so2.getV();
@@ -40,27 +41,50 @@ public class SlidingObjects extends Object implements Driver{
 		e = this.getE();
 		mu=super.getMu();
 		g=super.getG();
-		System.out.println("For Object 1");
-		if((v2-v1)*(x2-x1) < 0)
+		if(x2-x1 > 0 && v2 - v1 < 0)
 		{
-			double col_time = ((v1+v2)/(x2-x1));
-			double temp=0;
-			v1 = v1-mu*g*col_time;
-			v2 = v2-mu*g*col_time;
-			vcm = (m1*v1 + m2*v2)/(m1+m2);
-			v1f = (1+e)*vcm -e*v1;
-			if(t>col_time)
+			double col_time = (x2-x1)/(v1-v2);
+			t1 = v1/mu*g;
+			if(v2>=0)
 			{
-				plane(subchoice,ts,col_time);
-				this.setV(v1f);
-				this.setA(0);
-				plane(subchoice,ts, t-col_time);
+				if(t1 <= col_time)
+				{
+					System.out.println("Objects will not collide");
+				}
+				else
+				{
+					for(double i = 0; i < t; i+=ts)
+					{
+						if(i < col_time)
+						{
+							x1AtTime = v1*i+0.5*((-1)*mu*g)*i*i+x1;
+							x2AtTime = v2*i +0.5*(-1)*mu*g*i*i + x2;
+							System.out.println("coordinates of the object1 after "+i+" seconds is ("+ x1AtTime + ","+y1+").");
+							System.out.println("coordinates of the object2 after "+i+" seconds is ("+ x2AtTime + ","+y1+").");
+						}
+					}
+					v1 = v1-mu*g*col_time;
+					v2 = v2-mu*g*col_time;
+					vcm = (m1*v1 + m2*v2)/(m1+m2);
+					v1f = (1+e)*vcm -e*v1;
+					v2f = (1+e)*vcm -e*v2;
+					double xc1,xc2;
+					System.out.println("Objects will collide at " +col_time+ "seconds");
+					xc1 = v1*col_time+0.5*((-1)*mu*g)*col_time*col_time+x1;
+					xc2 = v2*col_time+0.5*((-1)*mu*g)*col_time*col_time+x2;
+					System.out.println("coordinates of the object1 after "+col_time+" seconds is ("+ xc1 + ","+y1+").");
+					System.out.println("coordinates of the object2 after "+col_time+" seconds is ("+ xc2 + ","+y1+").");
+					for(double i = col_time + ts; i < t; i+=ts)
+					{
+						double q = i-col_time;
+						x1AtTime = xc1 + v1f*q -0.5*mu*g*q*q;
+						x2AtTime = xc2 + v2f*q -0.5*mu*g*q*q;
+						System.out.println("coordinates of the object1 after "+i+" seconds is ("+ x1AtTime + ","+y1+").");
+						System.out.println("coordinates of the object2 after "+i+" seconds is ("+ x2AtTime + ","+y1+").");
+					}
+				}
 			}
-			else plane(subchoice,ts,t);
-		}
-		else
-		{
-			plane(subchoice,ts,t);
+			
 		}
 		
 	}
